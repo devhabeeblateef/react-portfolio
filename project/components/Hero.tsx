2a97dfbf4272cc18ab5e1c33fb591e180553c661
devhabeeb <77229868+devhabeeblateef@users.noreply.github.com>
2022-10-16 00:25:51 +0100

    CircleAnimation done

--- project/components/Hero.tsx
+++ project/components/Hero.tsx
@@ -1,20 +1,19 @@
 import React from 'react'
-import {Cursor, useTypewriter} from 'react-simple-typewriter'
+
+import BackgroundCircles from './BackgroundCircles'
 
 type Props = {}
 
 function Hero({}: Props) {
-    const [text, count] = useTypewriter({
-        words:["Hi", "Hello", "Bye"],
-        loop: true,
-        delaySpeed: 2000,
-    })
   return (
-    <div>
-        <p>{text}</p>
-        <Cursor cursorColor='#F7ABOA'/>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center 
+    text-center overflow-hidden'> 
+     <BackgroundCircles/>
+     <h1>
+      <span>Hello World!</span>
+     </h1>
     </div>
-  );
+  )
 }
 
 export default Hero
--- project/components/Home.tsx
+++ project/components/Home.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 export default function Home({}: Props) {
   return (
-    <div>
+    <div className='hidden md:inline-block'>
       <h1 className="leading-snug w-28 font-['Poppins'] font-bold ml-16 text-5xl">Habeeb Lateef Ayokunle</h1>
       <h3 className="font-['Secular-One'] font-extrabold ml-16 text-5xl mt-12">“</h3>
       <h5 className="ml-16 w-96 font-['Poppins']">
