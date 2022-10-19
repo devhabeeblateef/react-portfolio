0cfbea30b032a8d35c957c1107327ac929edd7c2
devhabeeb <77229868+devhabeeblateef@users.noreply.github.com>
2022-10-19 14:00:29 +0100

    Added Sanity.io config

---
+++ project/sanity/schemas/post.js
@@ -0,0 +1,42 @@
+export default {
+  name: 'pageInfo',
+  title: 'PageInfo',
+  type: 'document',
+  fields: [
+    {
+    name: "name",
+    title: "name",
+    type: "String"
+    },
+
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+
+    {
+      name: "heroImage",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: "profilePic",
+      title: "ProfilePic",
+      type: "image",
+      options: {
+        hotspot: true,
+      }
+    },
+
+    {
+      name: "email",
+      title: "Address",
+      type: "string",
+    },
+  ],
+}
